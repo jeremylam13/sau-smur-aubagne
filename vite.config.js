ba7413ad6baaec241ffd08a2a176f8964e7f74b5
Jérémy Lamiable <jeremylamiable@imac-1.home>
2026-05-31 22:36:30 +0200

    feat: publicDir + icones dans dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'Public',
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 2000,
